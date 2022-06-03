087d584d63818413238c328cf03534bf29e15081
Alin Florin <alin@qoffice.ro>
2022-06-03 11:39:26 +0300

    d

--- boot.py
+++ boot.py
@@ -1,7 +1,10 @@
 from flask import Flask
+import flask
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, Worldddd!</p>"
+@app.route("/api/filters/apply", methods=['POST'])
+def applyFilters():
+    raw_data = flask.request.get_data(False, False, False)
+    
+    return raw_data
